c2c1081678eadb9a2e5db234c7b66d967d4da523
Daniel <dagranados@estudiantec.cr>
2023-05-22 15:59:57 -0600

    Dirty Read 2

--- Dirty Read/SP_RegistrarFacturaRecoleccionDR1.sql
+++ Dirty Read/SP_RegistrarFacturaRecoleccionDR1.sql
@@ -45,83 +45,110 @@ BEGIN
 		montoAPagar MONEY
 	)
 
-	
-
-	INSERT INTO #viajesSelect (productor,total, recolector, montoRecoleccion, montoTratamiento, comision, viaje, descuento, montoAPagar) 
-	(SELECT locales.productorId, ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion), camiones.recolectorId, (sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion,sumasDesechosViajes.costosTratos / tCT.conversion, 
-	costosPasoRecoleccion.comisionEV / tCC.conversion, viajesRecoleccion.viajeId, 
-	(CASE 
-		WHEN ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) > (saldosDistribucion.montoSaldo / vc.localesCount) / tcs.conversion THEN (saldosDistribucion.montoSaldo / vc.localesCount) / tCS.conversion
-		ELSE ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion)
-	END ),((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) - (CASE 
-		WHEN ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) > (saldosDistribucion.montoSaldo / vc.localesCount) / tcs.conversion THEN (saldosDistribucion.montoSaldo / vc.localesCount) / tCS.conversion
-		ELSE ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion)
-	END )
-	FROM @viajes v
-	INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = v.viajeId   
-	INNER JOIN (SELECT localId, COUNT(localId) localesCount FROM @viajes vs INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = vs.viajeId GROUP BY localId) vc ON viajesRecoleccion.localId = vc.localId
-	INNER JOIN locales ON locales.localId = viajesRecoleccion.localId
-	INNER JOIN camiones ON camiones.camionId = viajesRecoleccion.camionId
-	INNER JOIN costosPasoRecoleccion ON viajesRecoleccion.recPasoId = costosPasoRecoleccion.recPasoId
-	INNER JOIN saldosDistribucion ON viajesRecoleccion.localId = saldosDistribucion.localId
-	INNER JOIN tiposDeCambio tCC ON costosPasoRecoleccion.monedaId = tCC.monedaCambioId
-	INNER JOIN tiposDeCambio tCS ON saldosDistribucion.monedaId = tCS.monedaCambioId
-	INNER JOIN direcciones ON locales.direccionId = direcciones.direccionId
-	INNER JOIN ciudades ON direcciones.ciudadId = ciudades.ciudadId
-	INNER JOIN estados ON estados.estadoId = ciudades.estadoId
-	INNER JOIN paises ON estados.paisId = paises.paisId
-	INNER JOIN 
-	(SELECT SUM(desechosPlantasLogs.cantidad) AS cantidadDesechoRecogido, SUM(desechosPlantasLogs.costoTrato) AS costosTratos, costosTratamiento.monedaId AS monedaCosto, 
-	viajesRecoleccion.viajeId as sumaViajeId FROM desechosPlantasLogs 
-	INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = desechosPlantasLogs.viajeId INNER JOIN costosTratamiento ON 
-	desechosPlantasLogs.costoTratoId = costosTratamiento.costoTratoId GROUP BY desechosPlantasLogs.viajeId, costosTratamiento.monedaId,  viajesRecoleccion.viajeId) AS sumasDesechosViajes 
-	ON sumasDesechosViajes.sumaViajeId = viajesRecoleccion.viajeId
-	INNER JOIN 
-	(SELECT SUM(desechosPorPaso.maxEsperado) AS cantidadEsperada, desechosPorPaso.recPasoId as sumaPasoId FROM desechosPorPaso 
-	INNER JOIN viajesRecoleccion ON viajesRecoleccion.recPasoId = desechosPorPaso.recPasoId GROUP BY desechosPorPaso.recPasoId) AS sumasDesechosPasos ON viajesRecoleccion.recPasoId = sumasDesechosPasos.sumaPasoId
-	INNER JOIN tiposDeCambio tCT ON sumasDesechosViajes.monedaCosto = tCT.monedaCambioId
-	WHERE costosPasoRecoleccion.areaEfectoId = (CASE 
-		WHEN costosPasoRecoleccion.objectTypeId = 1 THEN locales.direccionId
-		WHEN costosPasoRecoleccion.objectTypeId = 2 THEN direcciones.ciudadId
-		WHEN costosPasoRecoleccion.objectTypeId = 3 THEN ciudades.estadoId
-		WHEN costosPasoRecoleccion.objectTypeId = 4 THEN estados.paisId
-		ELSE
-		CASE 
-			WHEN (SELECT direccionId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.direccionId = locales.direccionId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
-			WHEN (SELECT ciudadId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.ciudadId = direcciones.ciudadId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
-			WHEN (SELECT estadoId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.estadoId = ciudades.estadoId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
-			WHEN (SELECT paisId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.paisId = estados.paisId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
-			ELSE NULL
-		END
-	END))
-	
+	/*
+	El programador puso Read Uncommitted en el nivel de isolación ya que 
+	quiere maximizar la concurrencia. El programador piensa que no deberían
+	ocurrir muchos rollbacks por lo que no se preocupa por el nivel de isolación.
+	*/
 	SET @InicieTransaccion = 0
 	IF @@TRANCOUNT=0 BEGIN
-		SET @InicieTransaccion = 1
-		SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+		SET @InicieTransaccion = 1;
+		SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 		BEGIN TRANSACTION		
 	END
 	
 	BEGIN TRY
 		SET @CustomError = 2001;
-		
+		-- T1 empieza la transacción
+		/*
+		El programador realizó el procesamiento al puro inicio de la transacción
+		para que el resto sea más fluido. Luego todo se extrae de esta tabla.
+		En esta tabla se hace la lectura del saldoActual y se distribuye para 
+		cada viaje.
+		*/
+		INSERT INTO #viajesSelect (productor,total, recolector, montoRecoleccion, montoTratamiento, comision, viaje, descuento, montoAPagar) 
+		(SELECT locales.productorId, ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion), camiones.recolectorId, (sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion,sumasDesechosViajes.costosTratos / tCT.conversion, 
+		costosPasoRecoleccion.comisionEV / tCC.conversion, viajesRecoleccion.viajeId, 
+		(CASE 
+			WHEN ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) > (saldosDistribucion.montoSaldo / vc.localesCount) / tcs.conversion THEN (saldosDistribucion.montoSaldo / vc.localesCount) / tCS.conversion
+			ELSE ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion)
+		END ),((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) - (CASE 
+			WHEN ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) > (saldosDistribucion.montoSaldo / vc.localesCount) / tcs.conversion THEN (saldosDistribucion.montoSaldo / vc.localesCount) / tCS.conversion
+			ELSE ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion)
+		END )
+		FROM @viajes v
+		INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = v.viajeId   
+		INNER JOIN (SELECT localId, COUNT(localId) localesCount FROM @viajes vs INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = vs.viajeId GROUP BY localId) vc ON viajesRecoleccion.localId = vc.localId
+		INNER JOIN locales ON locales.localId = viajesRecoleccion.localId
+		INNER JOIN camiones ON camiones.camionId = viajesRecoleccion.camionId
+		INNER JOIN costosPasoRecoleccion ON viajesRecoleccion.recPasoId = costosPasoRecoleccion.recPasoId
+		INNER JOIN saldosDistribucion ON viajesRecoleccion.localId = saldosDistribucion.localId
+		INNER JOIN tiposDeCambio tCC ON costosPasoRecoleccion.monedaId = tCC.monedaCambioId
+		INNER JOIN tiposDeCambio tCS ON saldosDistribucion.monedaId = tCS.monedaCambioId
+		INNER JOIN direcciones ON locales.direccionId = direcciones.direccionId
+		INNER JOIN ciudades ON direcciones.ciudadId = ciudades.ciudadId
+		INNER JOIN estados ON estados.estadoId = ciudades.estadoId
+		INNER JOIN paises ON estados.paisId = paises.paisId
+		INNER JOIN 
+		(SELECT SUM(desechosPlantasLogs.cantidad) AS cantidadDesechoRecogido, SUM(desechosPlantasLogs.costoTrato) AS costosTratos, costosTratamiento.monedaId AS monedaCosto, 
+		viajesRecoleccion.viajeId as sumaViajeId FROM desechosPlantasLogs 
+		INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = desechosPlantasLogs.viajeId INNER JOIN costosTratamiento ON 
+		desechosPlantasLogs.costoTratoId = costosTratamiento.costoTratoId GROUP BY desechosPlantasLogs.viajeId, costosTratamiento.monedaId,  viajesRecoleccion.viajeId) AS sumasDesechosViajes 
+		ON sumasDesechosViajes.sumaViajeId = viajesRecoleccion.viajeId
+		INNER JOIN 
+		(SELECT SUM(desechosPorPaso.maxEsperado) AS cantidadEsperada, desechosPorPaso.recPasoId as sumaPasoId FROM desechosPorPaso 
+		INNER JOIN viajesRecoleccion ON viajesRecoleccion.recPasoId = desechosPorPaso.recPasoId GROUP BY desechosPorPaso.recPasoId) AS sumasDesechosPasos ON viajesRecoleccion.recPasoId = sumasDesechosPasos.sumaPasoId
+		INNER JOIN tiposDeCambio tCT ON sumasDesechosViajes.monedaCosto = tCT.monedaCambioId
+		WHERE costosPasoRecoleccion.areaEfectoId = (CASE 
+			WHEN costosPasoRecoleccion.objectTypeId = 1 THEN locales.direccionId
+			WHEN costosPasoRecoleccion.objectTypeId = 2 THEN direcciones.ciudadId
+			WHEN costosPasoRecoleccion.objectTypeId = 3 THEN ciudades.estadoId
+			WHEN costosPasoRecoleccion.objectTypeId = 4 THEN estados.paisId
+			ELSE
+			CASE 
+				WHEN (SELECT direccionId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.direccionId = locales.direccionId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
+				WHEN (SELECT ciudadId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.ciudadId = direcciones.ciudadId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
+				WHEN (SELECT estadoId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.estadoId = ciudades.estadoId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
+				WHEN (SELECT paisId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.paisId = estados.paisId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
+				ELSE NULL
+			END
+		END));
+
 		--SELECT saldoId, montoSaldo FROM saldosDistribucion;
 
+		/*
+		El programador actualiza el saldo de primero para que otras transacciones
+		que se ejecuten concurrentemente lean un valor del saldo actualizado lo
+		más antes posible. Se arriesga a que haga rollback después, pero lo hace
+		por velocidad.
+		*/
+		-- T1 actualiza el saldo. 600-556 = 44
+		/*
+		T1 pone un lock exclusivo de escritura sobre saldosDistribución.
+		Esto significa que T2 no puede escribir, pero sí puede leer.
+		*/
 		WITH sumSaldo (descuentoTotal, localId) AS (
-			SELECT SUM(#viajesSelect.descuento) descuentoTotal, viajesRecoleccion.localId localId FROM #viajesSelect
-			INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = #viajesSelect.viaje
-			INNER JOIN saldosDistribucion ON saldosDistribucion.localId = viajesRecoleccion.localId
+			SELECT SUM(#viajesSelect.descuento) descuentoTotal,
+			viajesRecoleccion.localId localId FROM #viajesSelect
+			INNER JOIN viajesRecoleccion
+			ON viajesRecoleccion.viajeId = #viajesSelect.viaje
+			INNER JOIN saldosDistribucion
+			ON saldosDistribucion.localId = viajesRecoleccion.localId
 			GROUP BY viajesRecoleccion.localId
 		)
 		UPDATE saldosDistribucion
 		SET montoSaldo = montoSaldo - sumSaldo.descuentoTotal
-		FROM sumSaldo INNER JOIN saldosDistribucion ON saldosDistribucion.localId = sumSaldo.localId
+		FROM sumSaldo INNER JOIN saldosDistribucion
+		ON saldosDistribucion.localId = sumSaldo.localId
 
-		--SELECT saldoId, montoSaldo FROM saldosDistribucion;
 
 		waitfor delay '00:00:15'
-
+		-- Por razones del planificador, T1 espera y continúa T2.
 		
+		-- Cuando T1 continúa, llega a las validaciones. Un viaje no existe,
+		-- Por lo que debe hacer rollback. T1 hace rollback y el valor de los
+		-- el saldo se retorna al valor inicial de 600. T1 termina y T2 puede
+		-- continuar.
 		IF (SELECT COUNT(*) FROM @viajes v) != (SELECT COUNT(viaje) FROM #viajesSelect) BEGIN
 			RAISERROR ('VIAJES NO EXISTEN', 16, 1)
 		END
--- Dirty Read/SP_RegistrarFacturaRecoleccionDR2.sql
+++ Dirty Read/SP_RegistrarFacturaRecoleccionDR2.sql
@@ -46,81 +46,96 @@ BEGIN
 	)
 
 	
-
-	INSERT INTO #viajesSelect (productor,total, recolector, montoRecoleccion, montoTratamiento, comision, viaje, descuento, montoAPagar) 
-	(SELECT locales.productorId, ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion), camiones.recolectorId, (sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion,sumasDesechosViajes.costosTratos / tCT.conversion, 
-	costosPasoRecoleccion.comisionEV / tCC.conversion, viajesRecoleccion.viajeId, 
-	(CASE 
-		WHEN ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) > (saldosDistribucion.montoSaldo / vc.localesCount) / tcs.conversion THEN (saldosDistribucion.montoSaldo / vc.localesCount) / tCS.conversion
-		ELSE ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion)
-	END ),((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) - (CASE 
-		WHEN ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) > (saldosDistribucion.montoSaldo / vc.localesCount) / tcs.conversion THEN (saldosDistribucion.montoSaldo / vc.localesCount) / tCS.conversion
-		ELSE ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion)
-	END )
-	FROM @viajes v
-	INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = v.viajeId   
-	INNER JOIN (SELECT localId, COUNT(localId) localesCount FROM @viajes vs INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = vs.viajeId GROUP BY localId) vc ON viajesRecoleccion.localId = vc.localId
-	INNER JOIN locales ON locales.localId = viajesRecoleccion.localId
-	INNER JOIN camiones ON camiones.camionId = viajesRecoleccion.camionId
-	INNER JOIN costosPasoRecoleccion ON viajesRecoleccion.recPasoId = costosPasoRecoleccion.recPasoId
-	INNER JOIN saldosDistribucion ON viajesRecoleccion.localId = saldosDistribucion.localId
-	INNER JOIN tiposDeCambio tCC ON costosPasoRecoleccion.monedaId = tCC.monedaCambioId
-	INNER JOIN tiposDeCambio tCS ON saldosDistribucion.monedaId = tCS.monedaCambioId
-	INNER JOIN direcciones ON locales.direccionId = direcciones.direccionId
-	INNER JOIN ciudades ON direcciones.ciudadId = ciudades.ciudadId
-	INNER JOIN estados ON estados.estadoId = ciudades.estadoId
-	INNER JOIN paises ON estados.paisId = paises.paisId
-	INNER JOIN 
-	(SELECT SUM(desechosPlantasLogs.cantidad) AS cantidadDesechoRecogido, SUM(desechosPlantasLogs.costoTrato) AS costosTratos, costosTratamiento.monedaId AS monedaCosto, 
-	viajesRecoleccion.viajeId as sumaViajeId FROM desechosPlantasLogs 
-	INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = desechosPlantasLogs.viajeId INNER JOIN costosTratamiento ON 
-	desechosPlantasLogs.costoTratoId = costosTratamiento.costoTratoId GROUP BY desechosPlantasLogs.viajeId, costosTratamiento.monedaId,  viajesRecoleccion.viajeId) AS sumasDesechosViajes 
-	ON sumasDesechosViajes.sumaViajeId = viajesRecoleccion.viajeId
-	INNER JOIN 
-	(SELECT SUM(desechosPorPaso.maxEsperado) AS cantidadEsperada, desechosPorPaso.recPasoId as sumaPasoId FROM desechosPorPaso 
-	INNER JOIN viajesRecoleccion ON viajesRecoleccion.recPasoId = desechosPorPaso.recPasoId GROUP BY desechosPorPaso.recPasoId) AS sumasDesechosPasos ON viajesRecoleccion.recPasoId = sumasDesechosPasos.sumaPasoId
-	INNER JOIN tiposDeCambio tCT ON sumasDesechosViajes.monedaCosto = tCT.monedaCambioId
-	WHERE costosPasoRecoleccion.areaEfectoId = (CASE 
-		WHEN costosPasoRecoleccion.objectTypeId = 1 THEN locales.direccionId
-		WHEN costosPasoRecoleccion.objectTypeId = 2 THEN direcciones.ciudadId
-		WHEN costosPasoRecoleccion.objectTypeId = 3 THEN ciudades.estadoId
-		WHEN costosPasoRecoleccion.objectTypeId = 4 THEN estados.paisId
-		ELSE
-		CASE 
-			WHEN (SELECT direccionId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.direccionId = locales.direccionId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
-			WHEN (SELECT ciudadId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.ciudadId = direcciones.ciudadId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
-			WHEN (SELECT estadoId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.estadoId = ciudades.estadoId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
-			WHEN (SELECT paisId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.paisId = estados.paisId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
-			ELSE NULL
-		END
-	END))
-	
 	SET @InicieTransaccion = 0
 	IF @@TRANCOUNT=0 BEGIN
-		SET @InicieTransaccion = 1
-		SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+		SET @InicieTransaccion = 1;
+		SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 		BEGIN TRANSACTION		
 	END
 	
 	BEGIN TRY
 		SET @CustomError = 2001
-		
-				SELECT @@SPID AS SecondTransactionProcessID
-		EXEC sp_lock
+
+		-- T2 empieza su ejecución
+
+		/*
+		T2 lee el saldo que actualizó T1. Calcula los descuentos de los viajes
+		con base en este saldo. Como son dos viajes del mismo local, a cada uno 
+		le corresponde 22. 
+		*/
+		INSERT INTO #viajesSelect (productor,total, recolector, montoRecoleccion, montoTratamiento, comision, viaje, descuento, montoAPagar) 
+		(SELECT locales.productorId, ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion), camiones.recolectorId, (sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion,sumasDesechosViajes.costosTratos / tCT.conversion, 
+		costosPasoRecoleccion.comisionEV / tCC.conversion, viajesRecoleccion.viajeId, 
+		(CASE 
+			WHEN ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) > (saldosDistribucion.montoSaldo / vc.localesCount) / tcs.conversion THEN (saldosDistribucion.montoSaldo / vc.localesCount) / tCS.conversion
+			ELSE ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion)
+		END ),((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) - (CASE 
+			WHEN ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion) > (saldosDistribucion.montoSaldo / vc.localesCount) / tcs.conversion THEN (saldosDistribucion.montoSaldo / vc.localesCount) / tCS.conversion
+			ELSE ((sumasDesechosViajes.cantidadDesechoRecogido * costosPasoRecoleccion.costoRec / cantidadEsperada) / tCC.conversion + sumasDesechosViajes.costosTratos / tCT.conversion + costosPasoRecoleccion.comisionEV / tCC.conversion)
+		END )
+		FROM @viajes v
+		INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = v.viajeId   
+		INNER JOIN (SELECT localId, COUNT(localId) localesCount FROM @viajes vs INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = vs.viajeId GROUP BY localId) vc ON viajesRecoleccion.localId = vc.localId
+		INNER JOIN locales ON locales.localId = viajesRecoleccion.localId
+		INNER JOIN camiones ON camiones.camionId = viajesRecoleccion.camionId
+		INNER JOIN costosPasoRecoleccion ON viajesRecoleccion.recPasoId = costosPasoRecoleccion.recPasoId
+		INNER JOIN saldosDistribucion ON viajesRecoleccion.localId = saldosDistribucion.localId
+		INNER JOIN tiposDeCambio tCC ON costosPasoRecoleccion.monedaId = tCC.monedaCambioId
+		INNER JOIN tiposDeCambio tCS ON saldosDistribucion.monedaId = tCS.monedaCambioId
+		INNER JOIN direcciones ON locales.direccionId = direcciones.direccionId
+		INNER JOIN ciudades ON direcciones.ciudadId = ciudades.ciudadId
+		INNER JOIN estados ON estados.estadoId = ciudades.estadoId
+		INNER JOIN paises ON estados.paisId = paises.paisId
+		INNER JOIN 
+		(SELECT SUM(desechosPlantasLogs.cantidad) AS cantidadDesechoRecogido, SUM(desechosPlantasLogs.costoTrato) AS costosTratos, costosTratamiento.monedaId AS monedaCosto, 
+		viajesRecoleccion.viajeId as sumaViajeId FROM desechosPlantasLogs 
+		INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = desechosPlantasLogs.viajeId INNER JOIN costosTratamiento ON 
+		desechosPlantasLogs.costoTratoId = costosTratamiento.costoTratoId GROUP BY desechosPlantasLogs.viajeId, costosTratamiento.monedaId,  viajesRecoleccion.viajeId) AS sumasDesechosViajes 
+		ON sumasDesechosViajes.sumaViajeId = viajesRecoleccion.viajeId
+		INNER JOIN 
+		(SELECT SUM(desechosPorPaso.maxEsperado) AS cantidadEsperada, desechosPorPaso.recPasoId as sumaPasoId FROM desechosPorPaso 
+		INNER JOIN viajesRecoleccion ON viajesRecoleccion.recPasoId = desechosPorPaso.recPasoId GROUP BY desechosPorPaso.recPasoId) AS sumasDesechosPasos ON viajesRecoleccion.recPasoId = sumasDesechosPasos.sumaPasoId
+		INNER JOIN tiposDeCambio tCT ON sumasDesechosViajes.monedaCosto = tCT.monedaCambioId
+		WHERE costosPasoRecoleccion.areaEfectoId = (CASE 
+			WHEN costosPasoRecoleccion.objectTypeId = 1 THEN locales.direccionId
+			WHEN costosPasoRecoleccion.objectTypeId = 2 THEN direcciones.ciudadId
+			WHEN costosPasoRecoleccion.objectTypeId = 3 THEN ciudades.estadoId
+			WHEN costosPasoRecoleccion.objectTypeId = 4 THEN estados.paisId
+			ELSE
+			CASE 
+				WHEN (SELECT direccionId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.direccionId = locales.direccionId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
+				WHEN (SELECT ciudadId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.ciudadId = direcciones.ciudadId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
+				WHEN (SELECT estadoId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.estadoId = ciudades.estadoId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
+				WHEN (SELECT paisId FROM elementosPorRegion WHERE elementosPorRegion.regionId = costosPasoRecoleccion.areaEfectoId AND elementosPorRegion.paisId = estados.paisId) IS NOT NULL THEN costosPasoRecoleccion.areaEfectoId
+				ELSE NULL
+			END
+		END));
+	
 		SELECT saldoId, montoSaldo FROM saldosDistribucion;
 
+		/*
+		Cuando llega aquí, T2 no puede escribir porque T1 tiene el lock sobre
+		saldosDistribución, por lo que espera. Después de que T1 haga rollback,
+		T2 hace el update. Sin embargo, originalmente había leído que solo podía usar $44.
+		No obstante, ahora en el saldo a 600, por lo que de esos 600 solo gasta 44.
+		El saldo queda en 555. Aquí ocurre el Dirty read, ya que en realidad T2 debió de
+		haber tenido $600 disponibles.
+		*/
 		WITH sumSaldo (descuentoTotal, localId) AS (
-			SELECT SUM(#viajesSelect.descuento) descuentoTotal, viajesRecoleccion.localId localId FROM #viajesSelect
-			INNER JOIN viajesRecoleccion ON viajesRecoleccion.viajeId = #viajesSelect.viaje
-			INNER JOIN saldosDistribucion ON saldosDistribucion.localId = viajesRecoleccion.localId
+			SELECT SUM(#viajesSelect.descuento) descuentoTotal,
+			viajesRecoleccion.localId localId FROM #viajesSelect
+			INNER JOIN viajesRecoleccion
+			ON viajesRecoleccion.viajeId = #viajesSelect.viaje
+			INNER JOIN saldosDistribucion
+			ON saldosDistribucion.localId = viajesRecoleccion.localId
 			GROUP BY viajesRecoleccion.localId
 		)
 		UPDATE saldosDistribucion
 		SET montoSaldo = montoSaldo - sumSaldo.descuentoTotal
-		FROM sumSaldo INNER JOIN saldosDistribucion ON saldosDistribucion.localId = sumSaldo.localId
+		FROM sumSaldo INNER JOIN saldosDistribucion
+		ON saldosDistribucion.localId = sumSaldo.localId
 
 		SELECT saldoId, montoSaldo FROM saldosDistribucion;
-
 
 		IF (SELECT COUNT(*) FROM @viajes v) != (SELECT COUNT(viaje) FROM #viajesSelect) BEGIN
 			RAISERROR ('VIAJES NO EXISTEN', 16, 1)
