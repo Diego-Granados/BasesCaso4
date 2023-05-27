2f469fe045414ac26e7888654a8377b2839afb0a
Diego Granados <diegranados@estudiantec.cr>
2023-05-26 19:51:03 -0600

    Final Commit

--- Deadlock/DeadlockCon1.sql
+++ Deadlock/DeadlockCon1.sql
@@ -24,16 +24,3 @@ select * from itemsFactura;
 select * from itemsRecoleccion;
 select * from saldosDistribucion;
 select * from viajesRecoleccion;
-
-
-
-SELECT SUM(desechosPlantasLogs.cantidad) AS cantidadDesechoRecogido, SUM(desechosPlantasLogs.costoTrato) AS costosTratos, costosTratamiento.monedaId AS monedaCosto, 
-	v.viajeId as sumaViajeId FROM desechosPlantasLogs 
-	INNER JOIN viajesRecoleccion v ON v.viajeId = desechosPlantasLogs.viajeId INNER JOIN costosTratamiento ON 
-	desechosPlantasLogs.costoTratoId = costosTratamiento.costoTratoId GROUP BY desechosPlantasLogs.viajeId, costosTratamiento.monedaId, v.viajeId;
-
-SELECT viajeid, desechosPorPaso.maxEsperado AS cantidadEsperada, desechosPorPaso.recPasoId as sumaPasoId FROM desechosPorPaso 
-	INNER JOIN viajesRecoleccion ON viajesRecoleccion.recPasoId = desechosPorPaso.recPasoId ORDER BY viajeId
-
-	SELECT SUM(desechosPorPaso.maxEsperado) AS cantidadEsperada, desechosPorPaso.recPasoId as sumaPasoId FROM desechosPorPaso 
-	GROUP BY desechosPorPaso.recPasoId
