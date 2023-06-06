6aef0512fd87544e665a1910be7bd78e8bf51ca6
Daniel <dagranados@estudiantec.cr>
2023-06-06 15:44:45 -0600

    Users 3

--- Users/HierA.sql
+++ Users/HierA.sql
@@ -1,5 +1,9 @@
--- Hier A
+-- Usuario HierA: Tiene rol de viajesReader
 -- Un nivel superior niega acceso y un nivel inferior lo niega
 -- Rol de viajesReader niega acceso a pasosRecolección. Desde el usuario en la db, se permite el acceso por select
+-- Como el rol ya niega el acceso, no importa que se le haga permitido desde la tabla aún cuando el que dio permiso fue dbo.
+-- Sigue sin poder acceder.
 
 SELECT * FROM pasosRecoleccion;
+
+
