0f7e6b9442062d599696e7109b0328f5c8ee66bc
Diego Granados <diegranados@estudiantec.cr>
2023-06-06 17:22:25 -0600

    Fix query modelo

--- HierA.sql
+++
@@ -1,8 +0,0 @@
-
--- db_owner y viajesReader
-
-SELECT * FROM pasosRecoleccion;
-
-SELECT * FROM viajesRecoleccion;
-
--- Los denies del rol de viajesReader toman precedencia sobre los grants de db_owner.
--- Query Neo4J.sql
+++ Query Neo4J.sql
@@ -1,25 +1,42 @@
 SELECT locales.nombre, plantas.nombre, totales.cantidades, totales.viaje FROM 
 (SELECT totalLocales.localId As localidad, totalLocales.cantidad as cantidades, viajesRecoleccion.viajeId as viaje, viajesRecoleccion.plantaDestinoId as planta
 FROM viajesRecoleccion
-INNER JOIN (SELECT locales.localId as localId, SUM(desechosViajes.cantidad) as cantidad FROM locales
+INNER JOIN 
+(SELECT locales.localId as localId, (desechosViajes.cantidad) as cantidad 
+FROM locales
 INNER JOIN viajesRecoleccion ON locales.localId = viajesRecoleccion.localId
 INNER JOIN plantas ON viajesRecoleccion.plantaDestinoId = plantas.plantaId
-INNER JOIN (SELECT SUM(cantidad) as cantidad, viajeId FROM desechosPlantasLogs GROUP BY viajeId) desechosViajes ON desechosViajes.viajeId = viajesRecoleccion.viajeId
-GROUP BY locales.localId) totalLocales ON totalLocales.localId = viajesRecoleccion.localId) totales
+INNER JOIN 
+(SELECT SUM(cantidad) as cantidad, viajeId FROM desechosPlantasLogs GROUP BY viajeId) desechosViajes ON desechosViajes.viajeId = viajesRecoleccion.viajeId
+) totalLocales ON totalLocales.localId = viajesRecoleccion.localId) totales
 INNER JOIN locales ON totales.localidad = locales.localId
 INNER JOIN plantas ON plantas.plantaId = totales.planta
 ORDER BY locales.nombre
 
-select viajeId, recPasoId, localId, camionId, plantaOrigenId, plantaDestinoId, fechaInicio from viajesRecoleccion
-select nombre, localId from locales
-select nombre, plantaId from plantas
+
+SELECT locales.nombre, plantas.nombre, totales.cantidades, totales.viaje FROM 
+(SELECT vr.localId localidad, dpl.plantaId planta, SUM(dpl.cantidad) cantidades, vr.viajeId viaje FROM desechosPlantasLogs dpl
+INNER JOIN viajesRecoleccion vr ON dpl.viajeId = vr.viajeId
+GROUP BY vr.localId, dpl.plantaId) totales
+INNER JOIN locales ON totales.localidad = locales.localId
+INNER JOIN plantas ON plantas.plantaId = totales.planta
+ORDER BY locales.nombre
+
+
+
+select viajeId, recPasoId, localid, plantadestinoid, fechaInicio from viajesRecoleccion
+select viajeId, locales.nombre, plantas.nombre, fechaInicio from viajesRecoleccion
+INNEr join locales on locales.localId = viajesRecoleccion.localId
+inner join plantas on plantas.plantaId = viajesRecoleccion.plantaDestinoId
+select nombre from locales
+select nombre from plantas
 
 SELECT viajeId, SUM(cantidad) FROM desechosPlantasLogs GROUP BY viajeId ORDER BY viajeId
 
 
 
-select * from recolectores
-select * from productores
+select nombre from recolectores
+select nombre from productores
 
 select recolectores.nombre as recolector, productores.nombre as productor, totalesRecolectores.cantidadRecogida as cantidad
 FROM (select recolectores.recolectorId as recolector, productores.productorId as productor, SUM(desechosViajes.cantidad) as cantidadRecogida
